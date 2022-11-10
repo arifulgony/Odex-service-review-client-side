384bd145de06d9eef2ee7fce84b286916f8c41ee
arifulgony <arifulgony@gmail.com>
2022-11-10 23:29:14 +0600

    services card

--- src/Pages/Home/Services/ServiceCard.js
+++ src/Pages/Home/Services/ServiceCard.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-//import { FaBeer } from 'react-icons/fa';
 import { HiOutlineArrowNarrowRight } from "react-icons/hi";
 import './ServiceCard.css';
 import 'react-photo-view/dist/react-photo-view.css';
